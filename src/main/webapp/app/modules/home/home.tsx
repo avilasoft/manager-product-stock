ab8d7a4ad2b82e9e5c50d7ef01eaf6f99bbfbe97
brettbust <bsbustamantez@gmail.com>
2022-04-04 09:44:15 -0300

    prueba

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -18,7 +18,7 @@ export const Home = () => {
       <Col md="9">
         <h2>
           <Translate contentKey="home.title">Welcome, Java Hipster!</Translate>
-          Hello Brett
+          Hello Brett JHipster
         </h2>
         <p className="lead">
           <Translate contentKey="home.subtitle">This is your homepage</Translate>
