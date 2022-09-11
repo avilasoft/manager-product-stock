c91c79a5d6078b8bba9641faced716c112271246
wilever <wilevergomez@gmail.com>
2022-09-11 13:16:25 -0300

    v22

--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => (
     <Row>
       <Col md="12">
         <p>
-          <Translate contentKey="footer">Your footer</Translate>
+          <Translate contentKey="footer">Your footer v22</Translate>
         </p>
       </Col>
     </Row>
